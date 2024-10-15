d0e05f4296db4ca9359b08520bc253f0347b2872
Pedro Gabriel <53580539+salespedrogabriel@users.noreply.github.com>
2024-10-15 13:09:01 -0300

    Update raiz.h

--- Lab 02 - Raizes/raiz.h
+++ Lab 02 - Raizes/raiz.h
@@ -1,6 +1,8 @@
 #ifndef BISSECAO_H
 #define BISSECAO_H
 
+// Funcao que determina a raiz de uma funcao f(x) usando o metodo da bissecao 
+// com erro progressivo avaliado tenha precisao de 8 digitos
 int bissecao (double a, double b, double (*f) (double x), double* r);
 
 #endif
