6f97d54c780ce59c29cf13b1fc5f509205615117
Pedro Gabriel <53580539+salespedrogabriel@users.noreply.github.com>
2024-10-15 13:18:17 -0300

    Update sistlinear.h

--- Lab 03 - Sistlinear/sistlinear.h
+++ Lab 03 - Sistlinear/sistlinear.h
@@ -1,6 +1,8 @@
 #ifndef SISTLINEAR_H
 #define SISTLINEAR_H
 
+// Funcao que usa o metodo de Gauss com pivotamento
+// para determinar e preencher o vetor solucao x
 void gauss (int n, double** A, double* b, double* x);
 
 #endif
